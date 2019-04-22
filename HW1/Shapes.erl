048f69521887c2d51a742e4f65485915fc840d48
GuyAb <45210229+GuyAb@users.noreply.github.com>
2019-04-22 16:29:54 +0300

    Add files via upload

--- HW1/Shapes.erl
+++ HW1/Shapes.erl
@@ -10,7 +10,7 @@
 -author("guy ab").
 
 %% API
--export([shapesArea/1,squaresArea/1]).
+-export([shapesArea/1,squaresArea/1,trianglesArea/1,shapesFilter/1,shapesFilter2/1]).
 
 %%shapesArea - exported function that gets shapes lists and caculates their area sum
 % if the list is empty, the total area is zero
@@ -29,8 +29,8 @@ singleShapeArea({ellipse,{radius,Radius1,Radius2}}) when Radius1>0 and Radius2>0
 
 %% this one still doesnt check that we got all shapes
 squaresArea({shapes,[M]}) ->
-  squaresOnly = [x||{rectangle,x,x}<-M],
-  calcSquaresArea(squaresOnly).
+  squaresList = [x||{rectangle,x,x}<-M],
+  calcSquaresArea(squaresList).
 
 
 calcSquaresArea([]) -> 0;
@@ -39,12 +39,75 @@ calcSquaresArea([H|T]) when H>0 ->  H*H+calcSquaresArea(T).
 
 %% this one still doesnt check that we got all shapes
 trianglesArea({shapes,[M]}) ->
-  trianglesOnly = [{x,y}||{triangle,x,y}<-M],
-  calcTrianglesArea(trianglesOnly).
+    isTriangle = fun({shape,{kind,x,y}}) -> shape=:=triangle end,
+    trianglesList=lists:filter(isTriangle,M),
+    calcTrianglesArea(trianglesList).
 
 
 calcTrianglesArea([]) -> 0;
-calcSquaresArea([H|T]) when H>0 ->  H*H+calcSquaresArea(T).
+calcTrianglesArea([H|T]) when H>0 ->  singleShapeArea(H)+calcSquaresArea(T).
 
 
+shapesFilter(kindOfShape) when kindOfShape==rectangle or
+  kindOfShape==triangle or kindOfShape==ellipse ->
+  %%For every shape case, we return a Fun that filters the relvant shape and gets a Shape struct that
+  %% only contains these shapes
+  case kindOfShape of
+    rectangle ->
+      findRectangles = fun({shapes,[M]}) ->
+      isRectangle = fun({shape,{kind,x,y}}) -> shape=:=rectangle end,
+      newList=lists:filter(isRectangle,M),
+      {shapes,newList} end,
+      findRectangles;
+    trisngle ->
+      findTriangles = fun({shapes,[M]}) ->
+        isTriangle = fun({shape,{kind,x,y}}) -> shape=:=triangle end,
+        newList=lists:filter(isTriangle,M),
+        {shapes,newList} end,
+      findTriangles;
+    ellipse ->
+      findElipses = fun({shapes,[M]}) ->
+        isElipse = fun({shape,{kind,x,y}}) -> shape=:=elipse end,
+        newList=lists:filter(isElipse,M),
+        {shapes,newList} end,
+      findElipses
+end.
 
+
+shapesFilter2(kindOfShape)
+when kindOfShape==rectangle or
+kindOfShape==triangle or kindOfShape==ellipse ->
+%%For every shape case, we return a Fun that filters the relvant shape and gets a Shape struct that
+%% only contains these shapes
+case kindOfShape of
+  rectangle ->
+    findRectangles = fun({shapes,[M]}) ->
+    isRectangle = fun({shape,{kind,x,y}}) -> shape=:=rectangle end,
+    newList=lists:filter(isRectangle,M),
+    {shapes,newList} end,
+    findRectangles;
+  trisngle ->
+    findTriangles = fun({shapes,[M]}) ->
+    isTriangle = fun({shape,{kind,x,y}}) -> shape=:=triangle end,
+    newList=lists:filter(isTriangle,M),
+    {shapes,newList} end,
+    findTriangles;
+  ellipse ->
+    findElipses = fun({shapes,[M]}) ->
+    isElipse = fun({shape,{kind,x,y}}) -> shape=:=elipse end,
+    newList=lists:filter(isElipse,M),
+    {shapes,newList} end,
+    findElipses;
+  circle ->
+    findCircles = fun({shapes,[M]}) ->
+      isCircle = fun({shape,{kind,x,y}}) -> shape=:=elipse and x=:=y end,
+      newList=lists:filter(isCircle,M),
+      {shapes,newList} end,
+    findCircles;
+  square  ->
+    findSquares = fun({shapes,[M]}) ->
+      isSquare = fun({shape,{kind,x,y}}) -> shape=:=rectangle and x=:=y end,
+      newList=lists:filter(isSquare,M),
+      {shapes,newList} end,
+    findSquares
+end.
